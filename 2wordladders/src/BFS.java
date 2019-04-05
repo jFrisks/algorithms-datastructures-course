41fe002facdeb37bc85659eb884e750d3278f038
jfrisks <jo1875fr-s@student.lu.se>
2019-04-05 10:44:35 +0200

    BFS  update

--- 2wordladders/src/BFS.java
+++ 2wordladders/src/BFS.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
 public class BFS {
-    public String run()
+    public String run(Map<String, List<String>> V, String s, String t){
+        Map<String, Boolean> visited = new HashMap<>();
+
+        Deque<String> q = new ArrayDeque<String>();
+        q.addFirst(s);
+
+        //Making every node unvisited except s
+        for(String v: V.keySet()){
+            visited.put(v, false);
+        }
+        visited.put(s, true);
+
+        while(!q.isEmpty()){
+            String current = q.pop();
+
+            //Loop through neighbours
+            for(String neighbour: V.get(current)){
+                if(!visited.get(neighbour)){
+                    visited.put(neighbour, true);
+                    q.addLast(neighbour);
+                    predecessor(neighbour) = current;
+                }
+            }
+        }
+
+        return "";
+    }
+
+
 }
