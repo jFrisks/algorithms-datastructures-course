1c26dd08734b71a76c9d6be5917ab945cf9b1dd7
Elias Vernersson <DEVernersson@gmail.com>
2019-04-08 13:22:42 +0200

    Updated 'Main' class

--- 2wordladders/src/Main.java
+++ 2wordladders/src/Main.java
@@ -1,6 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<String, List<String>> graph = Parser.parse(bufferedReader);
+
+
     }
 }
