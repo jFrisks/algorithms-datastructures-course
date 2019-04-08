77ef421db9e706a41baea1b72af2a82414be6915
Elias Vernersson <DEVernersson@gmail.com>
2019-04-08 09:36:31 +0200

    Changed name of 'V' to 'graph'

--- 2wordladders/src/BFS.java
+++ 2wordladders/src/BFS.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class BFS {
-    public String run(Map<String, List<String>> V, String s, String t){
+    public String run(Map<String, List<String>> graph, String s, String t){
         Map<String, Boolean> visited = new HashMap<>();
         Map<String, String> predecessor = new HashMap<>();
 
@@ -9,16 +9,17 @@ public class BFS {
         q.addFirst(s);
 
         //Making every node unvisited except s
-        for(String v: V.keySet()){
+        for(String v: graph.keySet()){
             visited.put(v, false);
         }
+
         visited.put(s, true);
 
         while(!q.isEmpty()){
             String current = q.pop();
 
             //Loop through neighbours
-            for(String neighbour: V.get(current)){
+            for(String neighbour: graph.get(current)){
                 if(!visited.get(neighbour)){
                     visited.put(neighbour, true);
                     q.addLast(neighbour);
