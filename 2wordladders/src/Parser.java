fff29dd01b304056ed042c22fd90361ff98b9f2e
jfrisks <jo1875fr-s@student.lu.se>
2019-04-08 13:21:44 +0200

    Created basestructure for parser and its bufferedreader

--- 2wordladders/src/Parser.java
+++ 2wordladders/src/Parser.java
@@ -1,2 +1,10 @@
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.Map;
+
 public class Parser {
+
+    public static Map<String, List<String>> parse(BufferedReader br){
+        return null;
+    }
 }
