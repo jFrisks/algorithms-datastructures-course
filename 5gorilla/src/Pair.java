f5eaa6d1c3e80c58b4abc3e9c3bff56d7b9cb644
jfrisks <jo1875fr-s@student.lu.se>
2019-05-20 11:34:00 +0200

    Revert "Updated 5gorilla with new files"
    This reverts commit 4dfc737bc1c22a016096c293c1fc79e5cc5e2974.
    
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 5gorilla/src/Pair.java
@@ -0,0 +1,16 @@
+public class Pair {
+    private String X, Y;
+
+    public Pair(String X, String Y){
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public String getX() {
+        return X;
+    }
+
+    public String getY() {
+        return Y;
+    }
+}
--- 5gorilla/src/StringAlignmentDP.java
+++ 5gorilla/src/StringAlignmentDP.java
@@ -1,3 +1,11 @@
-public class StringAlignmentDP {
 
+public class StringAlignmentDP {
+    int[] makeTable(int n, int m) {
+        for(int j = 0; j < m; j++){
+            for(int i = 0; i < n; i++){
+
+            }
+        }
+        return null;
+    }
 }
