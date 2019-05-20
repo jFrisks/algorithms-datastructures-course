3bc8e24f0a2c26c23848b1c9c1b4d290cd4e0d69
jfrisks <jo1875fr-s@student.lu.se>
2019-05-20 14:09:41 +0200

    Created CostTable for String Alignment

--- 5gorilla/src/StringAlignmentDP.java
+++ 5gorilla/src/StringAlignmentDP.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.Map;
 
 public class StringAlignmentDP {
     /** PROBLEMS:
@@ -7,27 +9,53 @@ public class StringAlignmentDP {
      *
      * */
 
-    //MAP: cost.get(String.concat(wordX.charAt(i), wordY.charAt(j)))
-    //M
+    /** REFACORS:
+     * - Sorting in cost or getCost?
+     *
+     * */
 
-    int[][] makeTable(int n, int m, StringAlignmentParser parsed) {
+    int[][] makeTable(String wordX, String wordY, StringAlignmentParser parseCost) {
+        int n = wordX.length();
+        int m = wordY.length();
         int[][] OPTTable = new int[m][n];
+        wordX = " " + wordX;
+        wordY = " " + wordY;
 
-        for(int j = 0; j < m; j++){
-            for(int i = 0; i < n; i++){
+        for(int j = 0; j <= m; j++){
+            for(int i = 0; i <= n; i++){
                 //Handle outOfbOunds - if one index == 0 -> give starCost
-                if(i == 0 || j == 0) OPTTable[j][i]
-                int leftCost = parsed.starCost() + OPTTable[j-i][i];
-                int downCost = parsed.starCost() + OPTTable[j][i-1];
-                int diagonalCost = parsed.cost(i,j) + OPTTable[j-1][i-1];
-                int thisTotalBestPoints = Integer.max(diagonalCost, Integer.max(leftCost, downCost));
-                OPTTable[i][j] = thisTotalBestPoints;
+                if(i == 0 && j == 0) OPTTable[j][i] = parseCost.getCost("*");
+                else if(i == 0) OPTTable[j][i] = (j+1)*parseCost.getCost("*");
+                else if(j == 0) OPTTable[j][i] = (i+1)*parseCost.getCost("*");
+                else{
+                    int downCost = parseCost.getCost("*") + OPTTable[j-i][i];
+                    int leftCost = parseCost.getCost("*") + OPTTable[j][i-1];
+                    int diagonalCost = cost(i, j, parseCost, wordX, wordY) + OPTTable[j-1][i-1];
+                    int thisTotalBestPoints = Integer.max(diagonalCost, Integer.max(leftCost, downCost));
+                    OPTTable[i][j] = thisTotalBestPoints;
+                }
             }
         }
         return OPTTable;
     }
 
-    int cost(int i, int j){
+    private int cost(int i, int j, StringAlignmentParser parseCost, String wordX, String wordY) {
+        String keyValue = Character.toString(wordX.charAt(i)) + wordY.charAt(j);
+        return parseCost.getCost(keyValue);
+    }
 
+    void doStringAlignments(StringAlignmentParser parser) {
+        List<Pair> wordPairs = parser.getWordPairs();
+
+        for(Pair pair : wordPairs){
+            int[][] costTable = makeTable(pair.getX(), pair.getY(), parser);
+            String bestWordX = "";
+            String bestWordY = "";
+
+            //TODO: See comments below
+            //Gå igenom bästa väg
+                //skapa stringen undertiden
+            //printa strängarna
+        }
     }
 }
