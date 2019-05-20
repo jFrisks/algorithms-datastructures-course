f8122789affd51f4b6cc814af14fce5fe7604299
jfrisks <jo1875fr-s@student.lu.se>
2019-05-20 10:04:39 +0200

    Revert "Updated 5gorilla with new files"
    This reverts commit 4dfc737bc1c22a016096c293c1fc79e5cc5e2974.
    
    # Conflicts:
    #	.idea/workspace.xml

--- 4closestpair/src/ClosestPair.java
+++ 4closestpair/src/ClosestPair.java
@@ -2,14 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class ClosestPair {
-    //TODO: Change arraylist to array. This will
+    /** PROBLEMS to tackle
+     * - Dela
+     *
+     *
+     *
+     * OPTIMIZING
+     * - Fix n^2-func in basecase for calc minvaues.
+     * */
 
 
     final static int MINIMAL_DIVIDE = 3;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("./4closestpair/data/secret/4larger.in"));
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader br = new BufferedReader(new FileReader("./4closestpair/data/secret/6huger.in"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         List parsedPoints = parse(br);
 
@@ -42,24 +49,24 @@ public class ClosestPair {
     }
 
     public static double conquerAndDivide(List<PointInt> points) {
-        //points.sort(Comparator.comparingInt(PointInt::getX));
-        List<PointInt> pointsX = new ArrayList<>(points);
-        List<PointInt> pointsY = new ArrayList<>(points);
-        pointsX.sort(Comparator.comparingInt(PointInt::getX));
-        pointsY.sort(Comparator.comparingInt(PointInt::getY));
-        return closest(pointsX, pointsY);
+        points.sort(Comparator.comparingInt(PointInt::getX));
+        //List<PointInt> pointsX = new ArrayList<>(points);
+        //List<PointInt> pointsY = new ArrayList<>(points);
+        //pointsX.sort(Comparator.comparingInt(PointInt::getX));
+        //pointsY.sort(Comparator.comparingInt(PointInt::getY));
+        return closest(points);
     }
 
-    private static double closest(List<PointInt> pointsX, List<PointInt> pointsY) {
+    private static double closest(List<PointInt> points) {
         //BASE CASE
-        if(pointsX.size() < MINIMAL_DIVIDE){
+        if(points.size() < MINIMAL_DIVIDE){
             //TODO calc min value of remaining
             double minDist = Float.MAX_VALUE;
 
-            for(int p1I = 0; p1I < pointsX.size(); p1I++){
-                for(int p2I = p1I; p2I < pointsX.size(); p2I++){
-                    PointInt p1 = pointsX.get(p1I);
-                    PointInt p2 = pointsX.get(p2I);
+            for(int p1I = 0; p1I < points.size(); p1I++){
+                for(int p2I = p1I; p2I < points.size(); p2I++){
+                    PointInt p1 = points.get(p1I);
+                    PointInt p2 = points.get(p2I);
                     double tmpDist = p1.distance(p2);
                     if (tmpDist < minDist && !p2.equals(p1)) minDist = tmpDist;
                 }
@@ -78,67 +85,49 @@ public class ClosestPair {
         }
 
         //NOMRAL CASE
-        int medianXCoordIndex = Math.round(pointsX.size()/2);
-        double medianXCoord = pointsX.get(medianXCoordIndex).getX();
+        int medianXCoordIndex = Math.round(points.size()/2);
+        double medianXCoord = points.get(medianXCoordIndex).getX();
 
-        List<PointInt> leftPoints = pointsX.subList(0, medianXCoordIndex);
-        List<PointInt> rightPoints = pointsX.subList(medianXCoordIndex, pointsX.size());
-        Set<PointInt> leftPointSet = new HashSet<>(leftPoints); //O(n)
-        Set<PointInt> rightPointSet = new HashSet<>(rightPoints); //O(n)
-        List<PointInt> leftPointsY = new ArrayList<>(leftPoints.size());
-        List<PointInt> rightPointsY = new ArrayList<>(leftPoints.size());
-        for(PointInt point: pointsY){
-            if(leftPointSet.contains(point)) leftPointsY.add(point);
-            if(rightPointSet.contains(point)) rightPointsY.add(point);
-        }
+        List<PointInt> leftPoints = points.subList(0, medianXCoordIndex);
+        List<PointInt> rightPoints = points.subList(medianXCoordIndex, points.size());
 
-        double leftMin = closest(leftPoints, pointsY);
-        double rightMin = closest(rightPoints, pointsY);
+        double leftMin = closest(leftPoints);
+        double rightMin = closest(rightPoints);
         double partitionsMin = Double.min(leftMin, rightMin);
 
         //S.add - loop leftArrays element from median until dist-elemen.getX < partitionMin
         int leftBound = medianXCoordIndex;
-        while(pointsX.get(leftBound).getX() > medianXCoord - partitionsMin + 1 && leftBound > 0) {
+        while(points.get(leftBound).getX() > medianXCoord - partitionsMin + 1 && leftBound > 0) {
             leftBound -= 1;
         }
 
         //S add - Same for right array
         int rightBound = medianXCoordIndex;
-        while(pointsX.get(rightBound).getX() < medianXCoord + partitionsMin - 1 && rightBound < pointsX.size() - 1) {
+        while(points.get(rightBound).getX() < medianXCoord + partitionsMin - 1 && rightBound < points.size() - 1) {
             rightBound += 1;
         }
 
-        double stripMin = getStripMin(pointsX, pointsY, leftBound, rightBound);
+        double stripMin = getStripMin(points, leftBound, rightBound);
 
         return Double.min(partitionsMin, stripMin);
     }
 
-    private static double getStripMin(List<PointInt> pointsX, List<PointInt> pointsY, int leftBound, int rightBound) {
+    private static double getStripMin(List<PointInt> points, int leftBound, int rightBound) {
         /** OPTIMIZATION: Sort in the beginning of divide and conquer, not for each strip. We get extra log factor */
         double stripMin = Float.MAX_VALUE;
 
-        List<PointInt> Sx = pointsX.subList(leftBound, rightBound + 1);
-        Set<PointInt> SxSet = new HashSet<>(Sx);
-        List<PointInt> Sy = new ArrayList<>(Sx.size());
-        for(PointInt point: pointsY){
-            if(SxSet.contains(point)) Sy.add(point);
-        }
-
-        /* OLD Creating of Sy
         List<PointInt> S = points.subList(leftBound, rightBound + 1);
         S.sort(Comparator.comparingInt(PointInt::getY));
-        */
 
-
-        if (Sy.size() < 2) stripMin = Double.MAX_VALUE;
+        if (S.size() < 2) stripMin = Double.MAX_VALUE;
 
         //for each S -> check closest 15 points to the right of the array. Can only exist one point in each "partitionsMinbox". 4 boxes in width, makes 15 boxes to check
-        for (int i = 0; i < Sy.size() - 1; i++) {
-            int stripRightBound = (i + 15 > Sy.size() - 1) ? Sy.size() - 1 : i + 15;
-            List<PointInt> box = Sy.subList(i + 1, stripRightBound);
+        for (int i = 0; i < S.size() - 1; i++) {
+            int stripRightBound = (i + 15 > S.size() - 1) ? S.size() - 1 : i + 15;
+            List<PointInt> box = S.subList(i + 1, stripRightBound);
 
             for (PointInt other : box) {
-                double tmpDist = Sy.get(i).distance(other);
+                double tmpDist = S.get(i).distance(other);
                 if (tmpDist < stripMin) stripMin = tmpDist;
             }
         }
