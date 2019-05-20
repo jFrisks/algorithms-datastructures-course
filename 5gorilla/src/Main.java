040a419788593631925546bb8494fd165ee54dc3
Elias Vernersson <DEVernersson@gmail.com>
2019-05-20 14:10:14 +0200

    Created Main and fixed parsing

---
+++ 5gorilla/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("./5gorilla/data/secret/0mini.in"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        StringAlignmentParser parser = new StringAlignmentParser();
+
+        try {
+            parser.parse(br);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(parser.getCost("MV"));
+        System.out.println(parser.getCost("VM"));
+    }
+}
--- 5gorilla/src/StringAlignmentParser.java
+++ 5gorilla/src/StringAlignmentParser.java
@@ -1,2 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class StringAlignmentParser {
+
+    private final int SPACE_COST = -4;
+    private Map<String, Integer> combination = new HashMap<>();
+    private List<Pair> wordPairs = new ArrayList<>();
+
+    void parse(BufferedReader br) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String line = br.readLine();
+        String[] characters = line.split(" ");
+
+        while ((line = br.readLine()) != null) {
+            stringBuilder.append(line + " ");
+        }
+
+        String[] in = stringBuilder.toString().split(" ");
+        for (int i = 0; i < characters.length - 1; i++) {
+            for (int j = i; j < characters.length - 1; j++) {
+                String key = characters[i] + characters[j];
+                int value = Integer.parseInt(in[i + j * characters.length]);
+                combination.put(key, value);
+            }
+        }
+
+        combination.put("*", SPACE_COST);
+
+        int queries = characters.length*characters.length;
+
+        for (int i = characters.length*characters.length + 1; i < in.length - 1; i = i + 2) {
+            wordPairs.add(new Pair(in[i], in[i + 1]));
+        }
+    }
+
+    public int getCost(String key) {
+
+        String keyFlipped = new StringBuilder(key).reverse().toString();
+
+        if (key.charAt(0) - key.charAt(1) < 0) {
+            return combination.get(key);
+        } else {
+            return combination.get(keyFlipped);
+        }
+    }
+
+    public List<Pair> getWordPairs() {
+        return wordPairs;
+    }
 }
