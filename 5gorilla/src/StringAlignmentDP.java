392a8826d1f443e4aabb2ff1c8d978ba330677b5
jfrisks <jo1875fr-s@student.lu.se>
2019-05-20 12:25:00 +0200

    Revert "Updated 5gorilla with new files"
    This reverts commit 4dfc737bc1c22a016096c293c1fc79e5cc5e2974.
    
    # Conflicts:
    #	.idea/workspace.xml

--- 5gorilla/src/StringAlignmentDP.java
+++ 5gorilla/src/StringAlignmentDP.java
@@ -1,11 +1,33 @@
 
 public class StringAlignmentDP {
-    int[] makeTable(int n, int m) {
+    /** PROBLEMS:
+     * - Init value == 0, check so you replace correclty
+     *
+     *
+     *
+     * */
+
+    //MAP: cost.get(String.concat(wordX.charAt(i), wordY.charAt(j)))
+    //M
+
+    int[][] makeTable(int n, int m, StringAlignmentParser parsed) {
+        int[][] OPTTable = new int[m][n];
+
         for(int j = 0; j < m; j++){
             for(int i = 0; i < n; i++){
-
+                //Handle outOfbOunds - if one index == 0 -> give starCost
+                if(i == 0 || j == 0) OPTTable[j][i]
+                int leftCost = parsed.starCost() + OPTTable[j-i][i];
+                int downCost = parsed.starCost() + OPTTable[j][i-1];
+                int diagonalCost = parsed.cost(i,j) + OPTTable[j-1][i-1];
+                int thisTotalBestPoints = Integer.max(diagonalCost, Integer.max(leftCost, downCost));
+                OPTTable[i][j] = thisTotalBestPoints;
             }
         }
-        return null;
+        return OPTTable;
+    }
+
+    int cost(int i, int j){
+
     }
 }
